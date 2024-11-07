00f65837b6420f6554c73e0f6459adf1d110f169
theguffs <youssefabdelmalak01@gmail.com>
2024-11-07 18:38:57 +0100

    milestone 4 fatta ma senza progetti

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted(){
+    axios.get('http://localhost:8000/api/projects')
+    .then(response => {
+      this.projects = response.data;
+      console.log(this.projects);
+    })
+    .catch(error => {
+      console.error('non funziona')
+    });
+  },
+};
+
+</script>
+
+<template>
+  <div id="app">
+    <h1> progetti </h1>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
